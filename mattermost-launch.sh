5542a05c4e0b9a5b2127b615368d656313cf4747
victorklijmeij <victorklijmeij@users.noreply.github.com>
2018-01-28 15:20:30 +0100

    Update mattermost-launch.sh

--- mattermost-launch.sh
+++ mattermost-launch.sh
@@ -7,7 +7,10 @@ MM_PASSWORD=${DB_PASSWORD:-mostest}
 MM_DBNAME=${DB_DATABASE:-mattermost_test}
 
 echo -ne "Configure MySQL database connection..."
-sed -e 's#"DataSource": "mmuser:mostest@tcp(mysql:3306)/mattermost_test?charset=utf8mb4,utf8"#"DataSource": "'"$DB_USER:$DB_PASSWORD@tcp($DB_HOST:$DB_PORT_3306_TCP_PORT)/$DB_DATABASE?charset=utf8mb4,utf8"'"#' \
+#sed -e 's#"DataSource": "mmuser:mostest@tcp(mysql:3306)/mattermost_test?charset=utf8mb4,utf8"#"DataSource": "'"$DB_USER:$DB_PASSWORD@tcp($DB_HOST:$DB_PORT_3306_TCP_PORT)/$DB_DATABASE?charset=utf8mb4,utf8"'"#' \
+#    /opt/mattermost/config/config.json > /tmp/config.json
+
+sed -e 's#"DataSource": "postgres://mmuser:mostest@postgresql:5432/mattermost_test?sslmode=disable&connect_timeout=10"#"DataSource": "'"$DB_USER:$DB_PASSWORD@tcp($DB_HOST:$DB_PORT_3306_TCP_PORT)/$DB_DATABASE?charset=utf8mb4,utf8"'"#' \
     /opt/mattermost/config/config.json > /tmp/config.json
 
 cat /tmp/config.json >/opt/mattermost/config/config.json
